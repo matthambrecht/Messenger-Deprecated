a1a46817794975ec8bb7d85435d0f7a3f3307ee1
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-04-13 04:31:25 +0800

    Update 20160411032558_create_users.rb

--- db/migrate/20160411032558_create_users.rb
+++ db/migrate/20160411032558_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
       t.timestamps null: false
     end
   end
