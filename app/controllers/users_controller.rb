7fa790c235a07d1d661c6f67f2895065a66a9350
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-04-13 04:21:15 +0800

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/'
-    else
-      redirect_to '/signup'
-    end
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to '/'
+  else
+    redirect_to '/signup'
   end
+end
   private
   def user_params
     params.require(:user).permit(:username, :password)
