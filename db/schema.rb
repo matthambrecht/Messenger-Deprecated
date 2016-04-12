bb53226f1cbd8672f50e3c7094c7ea92850ef6b5
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-04-13 04:31:55 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409222637) do
+ActiveRecord::Schema.define(version: 20160411032558) do
 
   create_table "messages", force: :cascade do |t|
+    t.text     "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
