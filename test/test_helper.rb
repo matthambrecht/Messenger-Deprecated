5cd313b45d8d084a8b7e9c62e2d37f1f47a43d8a
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-04-25 05:14:09 +0800

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'coveralls'
 
-Coveralls.wear!
+Coveralls.wear!('rails')
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
